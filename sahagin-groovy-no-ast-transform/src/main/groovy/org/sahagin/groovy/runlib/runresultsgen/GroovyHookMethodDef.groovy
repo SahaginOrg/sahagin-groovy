71306d5ed5ada5a85ff7cdab023f3f68598f202c
NozomiIto <nozomi.ito@trident-qa.com>
2015-05-17 16:20:01 +0900

    Now prototyping

--- sahagin-groovy-no-ast-transform/src/main/groovy/org/sahagin/groovy/runlib/runresultsgen/GroovyHookMethodDef.groovy
+++ sahagin-groovy-no-ast-transform/src/main/groovy/org/sahagin/groovy/runlib/runresultsgen/GroovyHookMethodDef.groovy
@@ -1,7 +1,11 @@
+package org.sahagin.groovy.runlib.runresultsgen
+
 import java.io.File;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.sahagin.groovy.runlib.external.adapter.GroovyAdapterContainer
 import org.sahagin.groovy.runlib.external.adapter.geb.GebAdapter
 import org.sahagin.groovy.runlib.external.adapter.javalib.GroovyJavaLibAdapter
@@ -9,28 +13,38 @@ import org.sahagin.groovy.runlib.external.adapter.junit4.GroovyJUnit4Adapter
 import org.sahagin.groovy.runlib.external.adapter.spock.SpockAdapter
 import org.sahagin.groovy.runlib.srctreegen.GroovySrcTreeGenerator
 import org.sahagin.groovy.share.GroovyConfig
+import org.sahagin.report.HtmlReport;
+import org.sahagin.runlib.external.CaptureStyle;
 import org.sahagin.runlib.external.adapter.Adapter;
 import org.sahagin.runlib.external.adapter.AdapterContainer;
+import org.sahagin.runlib.runresultsgen.HookMethodDef;
+import org.sahagin.runlib.runresultsgen.HookMethodManager;
+import org.sahagin.runlib.runresultsgen.StackLineUtils;
 import org.sahagin.share.AcceptableLocales;
 import org.sahagin.share.CommonPath;
+import org.sahagin.share.IllegalDataStructureException;
+import org.sahagin.share.IllegalTestScriptException;
 import org.sahagin.share.Logging;
 import org.sahagin.share.SrcTreeChecker
 import org.sahagin.share.SysMessages;
+import org.sahagin.share.runresults.LineScreenCapture;
+import org.sahagin.share.runresults.RootMethodRunResult;
+import org.sahagin.share.runresults.RunFailure;
+import org.sahagin.share.runresults.StackLine;
 import org.sahagin.share.srctree.SrcTree;
+import org.sahagin.share.srctree.TestMethod;
 import org.sahagin.share.yaml.YamlConvertException;
 import org.sahagin.share.yaml.YamlUtils;
 
 class GroovyHookMethodDef {
     private static final String MSG_TEST_FRAMEWORK_NOT_FOUND = "testFramework not found: %s";
     private static Logger logger = Logging.getLogger(GroovyHookMethodDef.class.getName());
-    private static boolean initialized = false;
-    private static SrcTree srcTree;
+    private static HookMethodManager manager = null;
     
     static void initialize() {
-        if (initialized) {
+        if (manager != null) {
             return;
         }
-        logger.info("initialize");
         
         String configFilePath;
         String propValue = System.getProperty("sahagin.configPath")
@@ -41,6 +55,7 @@ class GroovyHookMethodDef {
         }
         GroovyConfig config = GroovyConfig.generateFromYamlConfig(new File(configFilePath));
         Logging.setLoggerEnabled(config.isOutputLog());   
+        logger.info("initialize");
         AcceptableLocales locales = AcceptableLocales.getInstance(config.getUserLocale());
         GroovyAdapterContainer.globalInitialize(locales, config.getTestFramework());
         SysMessages.globalInitialize(locales);
@@ -72,10 +87,29 @@ class GroovyHookMethodDef {
         if (config.getRootBaseReportIntermediateDataDir().exists()) {
             FileUtils.deleteDirectory(config.getRootBaseReportIntermediateDataDir());
         }
-
+        
         SrcTree srcTree = generateAndDumpSrcTree(config, locales);
+        manager = new HookMethodManager(srcTree, config);
+        
+        if (!config.isRunTestOnly()) {
+            // set up shutdown hook which generates HTML report
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    HtmlReport report = new HtmlReport();
+                    try {
+                        report.generate(config.getRootBaseReportIntermediateDataDir(),
+                                config.getRootBaseReportOutputDir());
+                    } catch (IllegalDataStructureException e) {
+                        throw new RuntimeException(e);
+                    } catch (IllegalTestScriptException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }       
     }
-
+    
     private static SrcTree generateAndDumpSrcTree(GroovyConfig config, AcceptableLocales locales) {
         // generate and dump srcTree
         GroovySrcTreeGenerator generator = new GroovySrcTreeGenerator(
@@ -85,6 +119,37 @@ class GroovyHookMethodDef {
         SrcTreeChecker.check(srcTree);
         YamlUtils.dump(srcTree.toYamlObject(), srcTreeFile);
         return srcTree;
+    }
+    
+    private static void initializedCheck() {
+        if (manager == null) {
+            throw new IllegalStateException("initialize first");
+        }
+    }
+    
+    public static void beforeMethodHook(
+            String hookedClassQualifiedName, String hookedMethodSimpleName) {
+        initializedCheck();
+        manager.beforeMethodHook(hookedClassQualifiedName, hookedMethodSimpleName);
+    }
+
+    public static void methodErrorHook(
+            String hookedClassQualifiedName, String hookedMethodSimpleName, Throwable e) {
+        initializedCheck();
+        manager.methodErrorHook(hookedClassQualifiedName, hookedMethodSimpleName, e);
+    }
+
+    public static void afterMethodHook(
+            String hookedClassQualifiedName, String hookedMethodSimpleName) {
+        initializedCheck();
+        manager.afterMethodHook(hookedClassQualifiedName, hookedMethodSimpleName);
+    }
+
+    public static void beforeCodeLineHook(String hookedClassQualifiedName, String hookedMethodSimpleName,
+            String hookedArgClassesStr, int line, int actualInsertedLine) {
+        initializedCheck();
+        manager.beforeCodeLineHook(hookedClassQualifiedName,
+                hookedMethodSimpleName, hookedArgClassesStr, line, actualInsertedLine);
     }
 
 }
